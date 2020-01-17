e7de4f00872f129ad02484e13f4f2849b4f8dc55
ashoshosho <svdovnar3@gmail.com>
2020-01-17 12:11:16 +0300

    actual gameplay with 1st level

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -7,7 +7,8 @@ public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
     AudioSource audioSource;
-    
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
 
     // Use this for initialization
     void Start () {
@@ -21,11 +22,27 @@ public class Rocket : MonoBehaviour {
         Thrust();
         Rotate();
 	}
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
+            case "Fuel":
+                print("Fuel");
+                break;
+            default:
+                print("Dead");
+                break;
+           
+        }
+    }
     private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space)) // can thrust while rotate
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
@@ -39,14 +56,15 @@ public class Rocket : MonoBehaviour {
     private void Rotate()
     {
         rigidBody.freezeRotation = true;
-
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+        
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
 
         rigidBody.freezeRotation = false;
