b892ab5e6f05e670cd27acf178179b0f34a62466
ashoshosho <svdovnar3@gmail.com>
2020-02-04 21:33:23 +0300

    Vector shiiit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+
+	[SerializeField] Vector3 movementVector;
+	[Range(0, 1)] [SerializeField] float movementFactor;
+	Vector3 startingPos;
+	
+	void Start () 
+	{
+		startingPos = transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 offset = movementVector * movementFactor;
+		transform.position = startingPos + offset;
+	}
+}
